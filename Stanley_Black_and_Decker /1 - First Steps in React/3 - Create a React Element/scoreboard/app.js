dc124a4faeb55ea484d8bf3fe2c81ba427b5b9a6
inunotaisho26 <ebfairweather@gmail.com>
2019-06-21 09:39:48 -0400

    studying props

--- Stanley_Black_and_Decker /1 - First Steps in React/3 - Create a React Element/scoreboard/app.js
+++ Stanley_Black_and_Decker /1 - First Steps in React/3 - Create a React Element/scoreboard/app.js
@@ -1,46 +1,46 @@
-// const Header = () => {
-//     return (
-//         <header>
-//             <h1>Scoreboard</h1>
-//             <span className="stats">Players: 1</span>
-//         </header>
-//     );
-// }
-
-const Player = () => {
+const Header = () => {
     return (
-        <div className="player">
-            <span className="player-name">
-                Ethan
-            </span>
-            <Counter />
-        </div>
-    )
-}
-
-// const Counter = () => {
-//     return (
-//         <div className="counter">
-//             <button className="counter-action decrement"> - </button>
-//             <span className="counter-score">35</span>
-//             <button className="counter-action increment"> + </button>
-//         </div>
-//     )
-// }
-
-const App = () => {
+      <header>
+        <h1>Scoreboard</h1>
+        <span className="stats">Players: 1</span>
+      </header>
+    );
+  }
+  
+  const Player = () => {
     return (
-        <div className="scoreboard" >
-            <Header />
-
-            <Player />
-        </div>
-    )
-}
-
-ReactDOM.render(
+      <div className="player">
+        <span className="player-name">
+          Ethan
+        </span>
+  
+        <Counter />
+      </div>
+    );
+  }
+  
+  const Counter = () => {
+    return (
+      <div className="counter">
+        <button className="counter-action decrement"> - </button>
+        <span className="counter-score">35</span>
+        <button className="counter-action increment"> + </button>
+      </div>
+    );
+  }
+  
+  const App = () => {
+    return (
+      <div className="scoreboard">
+        <Header />
+  
+        {/* Players list */}
+        <Player />
+      </div>
+    );
+  }
+  
+  ReactDOM.render(
     <App />,
     document.getElementById('root')
-);
-
-console.log(title);
+  );
